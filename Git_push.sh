d7756d28a271f41d3dc204cc812fc44069baff16
sriengchhunchheang <64910132@go.buu.ac.th>
2023-05-10 10:26:04 +0700

    add tutorail

--- Git_push.sh
+++ Git_push.sh
@@ -2,6 +2,11 @@
 MESSAGE="$*"
 git add .
 git commit -m "$MESSAGE"
-git push
+# git push
 
+
+
+## alias gitpush="/c/Users/60050158/Desktop/BDH/Bentoml_Yatai/Git_push.sh"     # How to save the script to use it as cmd. 
+## using: Reload shell startup file by: source ~/.bashrc
+## using: gitpush "message" to push to github  
 
